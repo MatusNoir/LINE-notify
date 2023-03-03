608a127af6fa9e890780d873e53ac694cccc633c
MatusNoir <noriyou510@yahoo.co.jp>
2023-03-03 18:51:18 +0900

    delete the information that no longer require

--- monthly/monthly_report.py
+++ monthly/monthly_report.py
@@ -1,8 +1,9 @@
 import requests
 
-url = "https://notify-api.line.me/api/notify" 
+url = "https://notify-api.line.me/api/notify"
+# input token from Line Notify Service when you connect first 
 token = "9T6wLfV6Knmk225oaJyDz8C30vqJ72xUPmVNQLveK3l"
 headers = {"Authorization" : "Bearer "+ token} 
-message =  {"\n【注意】13:30までです.\nhttps://ryohin.ccms.works-hi.co.jp/cws/mbl/MblActLogin"}
-payload = {"message" :  message} 
+message =  {"type massage here"}
+payload = {"Wow" :  message} 
 r = requests.post(url, headers = headers, params=payload)
